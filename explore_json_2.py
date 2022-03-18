38049d090600f60d83622d46106ed131a49a1251
Mary Hagan Chandler <mary_hagan1@baylor.edu>
2022-03-17 22:18:17 -0500

    Completed json_1 and json_2

--- explore_json_2.py
+++ explore_json_2.py
@@ -1,6 +1,6 @@
 import json
 
-infile = open("eq_data_1_day_m1.json","r")
+infile = open("eq_data_30_day_m1.json","r")
 outfile = open("readable_eq_data.json","w")
 
 
@@ -16,7 +16,7 @@ list_of_eqs = eq_data["features"]
     #This will return the value of features, which is a list of earthquakes.
 
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
     #You can create multiple lists in one line.
 
 for eq in list_of_eqs:
@@ -24,10 +24,11 @@ for eq in list_of_eqs:
     mag = eq["properties"]["mag"]
     lon = eq["geometry"]["coordinates"][0]
     lat = eq["geometry"]["coordinates"][1]
+    title = eq["properties"]["title"]
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
-
+    hover_texts.append(title)
 
 print(mags[:10])
 print(lons[:10])
@@ -38,8 +39,26 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 
-data = [Scattergeo(lon = lons,lat = lats)]
-                   #This is the argument for the function Scattergeo
+data = [
+    {"type":"scattergeo",       #Scattergeo is a type of plot, which is based on a world map.
+    "lon":lons,             #These lists all have to have the same number of elements
+    "lat":lats,             #"                          "
+    "text":hover_texts,     #"                          "
+    "marker": {
+        "size":[5*mag for mag in mags],
+        "color":mags,
+        "colorscale":"Viridis",
+        "reversescale":True,        #switches the colors to decreasing instead of increasing
+        "colorbar":{"title":"Magnitude"},      #the title of the colorscale will be Magnitude
+    },
+    }]
+
+#We can change the scale of the dots by using list comprehension. You could use a for loop to multiply
+#   each element of the list by a number, but it's faster to use LC.
+
+#Three parts to list comprehension:     [expression     iteration       condition]
+#Iterate is like our for loop, the expression is our i*5 thing, and the condition is optional
+
 
 my_layout = Layout(title = "Global Earthquakes")
 
