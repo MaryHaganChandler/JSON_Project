5d01818112a9e2b5dd256e34ab5cc86ac89f0aa1
Mary Hagan Chandler <mary_hagan1@baylor.edu>
2022-03-18 23:26:58 -0500

    Completed map 2, other than hovertext

--- schools.py
+++ schools.py
@@ -3,6 +3,7 @@ Process the JSON file named univ.json. Create 3 maps per instructions
 below.
 The size of the point on the map should be based on the size
 of total enrollment.
+^^^^^^^^^^^^^^^^^^^^^^^DO THIS^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^DO THIS
 Display only those schools that are part of the
 ACC, Big 12, Big Ten, Pac-12 and SEC divisons (refer to valueLabels.csv
 file).
@@ -31,7 +32,9 @@ univ_file = open("univ.json","r")
 univ_data = json.load(univ_file)
     #univ_data is a LIST
 
-list_of_univs, lons, lats = [], [], []
+list_of_univs1, lons1, lats1 = [], [], []
+list_of_univs2, lons2, lats2 = [], [], []
+list_of_univs3, lons3, lats3 = [], [], []
 grad_rate_women, black_aa_enroll, total_price_instate = [], [], []
 
 relevant_conferences = [102,107,108,127,180]
@@ -40,6 +43,9 @@ list_length = len(univ_data)
 
 
 map_1_index = 0
+map_2_index = 0
+map_3_index = 0
+
 while map_1_index < list_length:
     if univ_data[map_1_index]["NCAA"]["NAIA conference number football (IC2020)"] in relevant_conferences:
         if univ_data[map_1_index]["Graduation rate  women (DRVGR2020)"] > 50:
@@ -48,28 +54,51 @@ while map_1_index < list_length:
             lat = univ_data[map_1_index]["Latitude location of institution (HD2020)"]
             grad = univ_data[map_1_index]["Graduation rate  women (DRVGR2020)"]
 
-            list_of_univs.append(univs)
-            lons.append(lon)
-            lats.append(lat)
+            list_of_univs1.append(univs)
+            lons1.append(lon)
+            lats1.append(lat)
             grad_rate_women.append(grad)
 
+        if univ_data[map_2_index]["Percent of total enrollment that are Black or African American (DRVEF2020)"] > 10:
+            univs = univ_data[map_2_index]["instnm"]
+            lon = univ_data[map_2_index]["Longitude location of institution (HD2020)"]
+            lat = univ_data[map_2_index]["Latitude location of institution (HD2020)"]
+            enroll = univ_data[map_2_index]["Percent of total enrollment that are Black or African American (DRVEF2020)"]
+
+            list_of_univs2.append(univs)
+            lons2.append(lon)
+            lats2.append(lat)
+            black_aa_enroll.append(enroll)
+            
     map_1_index += 1
+    map_2_index += 1
+    map_3_index += 1
+
 
 #print(list_of_univs)
 #print(lons)
 #print()
 #print(lats)
 #print(grad_rate_women)
+#print(list_of_univs1)
+#print()
+#print(list_of_univs2)
+#print(black_aa_enroll)
+
+
+
 
 
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
+
+"""--------MAP 2--------"""
 map_1_data = [
     {"type":"scattergeo",
-    "lon":lons,
-    "lat":lats,
-    "text":list_of_univs,
+    "lon":lons1,
+    "lat":lats1,
+    "text":list_of_univs1,
     "marker": {
         "size":[0.2*grad for grad in grad_rate_women],
         "color":grad_rate_women,
@@ -78,28 +107,32 @@ map_1_data = [
     },
     }]
 
-
-
-
-"""
-map_1_data = [
-    {"type":"scattergeo",
-    "lon":lons,
-    "lat":lats,
-    "text":list_of_univs,
-    "marker": {
-        #"size":
-        #"color":
-        "colorscale":"Viridis",
-        "reversescale":True,
-        "colorbar":{"title":"Percentage"},
-    },
-    }]
-"""
-
 map_1_layout = Layout(title = "Graduation Rate for Women - Above 50%")
 
 fig = {"data":map_1_data, "layout": map_1_layout}
 
-
 offline.plot(fig, filename = "Graduation_Rate_For_Women.html")
+
+
+
+
+
+"""--------MAP 2--------"""
+map_2_data = [
+    {"type":"scattergeo",
+    "lon":lons2,
+    "lat":lats2,
+    "text":list_of_univs2,
+    "marker": {
+        "size":[1*enroll for enroll in black_aa_enroll],
+        "color":black_aa_enroll,
+        "colorscale":"magenta",
+        "colorbar":{"title":"Percentage of Total Enrollment"},
+    },
+    }]
+
+map_2_layout = Layout(title = "Black and African American Enrollment - Above 10%")
+
+fig = {"data":map_2_data, "layout": map_2_layout}
+
+offline.plot(fig, filename = "Black_And_African_American_Enrollment.html")
